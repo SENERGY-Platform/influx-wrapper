6837635bbdffc1e549ffc55035b14f7494c7f38a
franzmueller <franzmueller@users.noreply.github.com>
2020-07-02 11:43:30 +0200

    add test for influx.GetLatestValues

--- pkg/influx/influx_test.go
+++ pkg/influx/influx_test.go
@@ -17,7 +17,11 @@
 package influx
 
 import (
+	"errors"
 	"github.com/SENERGY-Platform/influx-wrapper/pkg/configuration"
+	"github.com/SENERGY-Platform/influx-wrapper/pkg/tests/services"
+	influxLib "github.com/orourkedd/influxdb1-client"
+	"github.com/orourkedd/influxdb1-client/models"
 	"testing"
 )
 
@@ -47,4 +51,249 @@ func TestInflux(t *testing.T) {
 			}
 		})
 	})
+
+	t.Run("GetLatestValues", func(t *testing.T) {
+		influxClientMock := services.NewClientMock()
+		influxClient := Influx{
+			config: &configuration.ConfigStruct{
+				Debug: true,
+			},
+			client: &influxClientMock,
+		}
+
+		t1 := "2000-01-01T00:00:00.000Z"
+		t2 := "2000-01-02T00:00:00.000Z"
+		db := "db"
+		v1 := 1
+		v2 := 2
+
+		t.Run("single", func(t *testing.T) {
+			influxClientMock.SetQueryResponse(&influxLib.Response{
+				Results: []influxLib.Result{
+					{
+						Series: []models.Row{
+							{
+								Name:    "m1",
+								Columns: []string{"time", "c1"},
+								Values: [][]interface{}{
+									{t1, v1},
+								},
+							},
+						},
+					},
+				},
+			}, nil)
+			measurementColumnPair := MeasurementColumnPair{
+				Measurement: "m1",
+				ColumnName:  "c1",
+			}
+			t.Run("normal", func(t *testing.T) {
+				actual, err := influxClient.GetLatestValue(db, measurementColumnPair)
+				expect := TimeValuePair{
+					Time:  &t1,
+					Value: 1,
+				}
+				if err != nil {
+					t.Error(err.Error())
+					return
+				}
+				if !timeValuePairEquals(actual, expect) {
+					t.Fail()
+				}
+			})
+			t.Run("error", func(t *testing.T) {
+				testErr := errors.New("random err")
+				influxClientMock.SetQueryResponse(&influxLib.Response{}, testErr)
+				_, err := influxClient.GetLatestValue(db, measurementColumnPair)
+				if err != testErr {
+					t.Fail()
+				}
+			})
+			t.Run("no result", func(t *testing.T) {
+				influxClientMock.SetQueryResponse(&influxLib.Response{}, nil)
+				_, err := influxClient.GetLatestValue(db, measurementColumnPair)
+				if err != ErrNULL {
+					t.Fail()
+				}
+			})
+			t.Run("no values", func(t *testing.T) {
+				influxClientMock.SetQueryResponse(&influxLib.Response{
+					Results: []influxLib.Result{
+						{
+							Series: []models.Row{
+								{
+									Name:    "m1",
+									Columns: []string{"time", "c1"},
+									Values:  [][]interface{}{},
+								},
+							},
+						},
+					},
+				}, nil)
+				_, err := influxClient.GetLatestValue(db, measurementColumnPair)
+				if err != ErrNULL {
+					t.Fail()
+				}
+			})
+		})
+		t.Run("multi", func(t *testing.T) {
+			influxClientMock.SetQueryResponse(&influxLib.Response{
+				Results: []influxLib.Result{
+					{
+						Series: []models.Row{
+							{
+								Name:    "m1",
+								Columns: []string{"time", "c1", "c2"},
+								Values: [][]interface{}{
+									{t1, v1, nil},
+								},
+							},
+							{
+								Name:    "m2",
+								Columns: []string{"time", "c1", "c2"},
+								Values: [][]interface{}{
+									{t2, nil, v2},
+								},
+							},
+						},
+					},
+				},
+			}, nil)
+			measurementColumnPairs := []MeasurementColumnPair{
+				{
+					Measurement: "m1",
+					ColumnName:  "c1",
+				},
+				{
+					Measurement: "m2",
+					ColumnName:  "c2",
+				},
+			}
+			t.Run("normal", func(t *testing.T) {
+				expected := []TimeValuePair{
+					{
+						Time:  &t1,
+						Value: v1,
+					},
+					{
+						Time:  &t2,
+						Value: v2,
+					},
+				}
+				actual, err := influxClient.GetLatestValues(db, measurementColumnPairs)
+				if err != nil {
+					t.Fail()
+					return
+				}
+				if !timeValuePairListEquals(expected, actual) {
+					t.Fail()
+				}
+			})
+			t.Run("series missing", func(t *testing.T) {
+				influxClientMock.SetQueryResponse(&influxLib.Response{
+					Results: []influxLib.Result{
+						{
+							Series: []models.Row{
+								{
+									Name:    "m1",
+									Columns: []string{"time", "c1", "c2"},
+									Values: [][]interface{}{
+										{t1, v1, nil},
+									},
+								},
+							},
+						},
+					},
+				}, nil)
+				expected := []TimeValuePair{
+					{
+						Time:  &t1,
+						Value: v1,
+					},
+					{
+						Time:  nil,
+						Value: nil,
+					},
+				}
+				actual, err := influxClient.GetLatestValues(db, measurementColumnPairs)
+				if err != nil {
+					t.Fail()
+					return
+				}
+				if !timeValuePairListEquals(expected, actual) {
+					t.Fail()
+				}
+			})
+			t.Run("column missing", func(t *testing.T) {
+				influxClientMock.SetQueryResponse(&influxLib.Response{
+					Results: []influxLib.Result{
+						{
+							Series: []models.Row{
+								{
+									Name:    "m1",
+									Columns: []string{"time", "c1", "c2"},
+									Values: [][]interface{}{
+										{t1, v1, nil},
+									},
+								},
+								{
+									Name:    "m2",
+									Columns: []string{"time", "c1", "c2"},
+									Values: [][]interface{}{
+										{t1, v1, nil},
+									},
+								},
+							},
+						},
+					},
+				}, nil)
+				expected := []TimeValuePair{
+					{
+						Time:  &t1,
+						Value: v1,
+					},
+					{
+						Time:  &t1,
+						Value: nil,
+					},
+				}
+				actual, err := influxClient.GetLatestValues(db, measurementColumnPairs)
+				if err != nil {
+					t.Fail()
+					return
+				}
+				if !timeValuePairListEquals(expected, actual) {
+					t.Fail()
+				}
+			})
+		})
+	})
+}
+
+func timeValuePairEquals(p1 TimeValuePair, p2 TimeValuePair) bool {
+	if p1.Value != p2.Value {
+		return false
+	}
+	if p1.Time == nil {
+		if p2.Time != nil {
+			return false
+		}
+		return true
+	}
+	if *p1.Time != *p2.Time {
+		return false
+	}
+	return true
+}
+
+func timeValuePairListEquals(l1 []TimeValuePair, l2 []TimeValuePair) bool {
+	if len(l1) != len(l2) {
+		return false
+	}
+	for i := range l1 {
+		if !timeValuePairEquals(l1[i], l2[i]) {
+			return false
+		}
+	}
+	return true
 }
